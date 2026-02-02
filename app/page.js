68e4c894e529e7ccf087085dbe56aa963b1df74a
aziz kchaou <mohamedaziz.kchaou@insat.ucar.tn>
2026-02-02 13:04:44 +0100

    app layout

--- app/page.js
+++ app/page.js
@@ -2,8 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    
-      <Button variant="secondary">Subscribe</Button>
-    
+    <Button variant="destructive">Subscribe</Button>
   );
 }
